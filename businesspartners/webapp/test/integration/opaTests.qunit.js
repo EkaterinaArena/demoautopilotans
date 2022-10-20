1dc1e0a42d87c4061948a8530a0d912d8d281b55
Ekaterina ARENA <ekaterina.arena@sap.com>
2022-10-20 11:46:30 +0000

    First commit. Version 0.0.1

---
+++ businesspartners/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["demo/businesspartners/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ businesspartners/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"demo/businesspartners/test/unit/controller/Suppliers.controller"
+], function () {
+	"use strict";
+});
